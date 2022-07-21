5997a0b40ef08083f4fedda704a274d86e45648d
julikharisma <45221345+julikharisma@users.noreply.github.com>
2022-07-21 23:45:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <style>
+     .picture {
+     border: 5px solid black;
+     border-radius: 100px;
+     margin: auto;
+     }
+     .block {
+        height: 50px;
+        width: 50px;
+        
+        
+     }
+     .inline {
+         width: 80px;  
+         height: 10px;
+         background: green;
+         border-radius: 10cm;
+         
+     }
+
+    </style>
+</head>
+<body>
+   <h1 style="text-align: center;">Juli Kharisma</h1>
+   <link rel="stylesheet" type="text/css"href="style2.css">
+   <div style= "text-align: center;">
+    <img class="picture" src="./lawu.jpg"width=225" height="225">
+   </div>
+   
+   <p style="text-align: center;">Perkenalkan nama saya Juli Kharisma, saya sedang mengikuti DTS Kominfo class front end, Harapan saya mampu menyerap semua ilmu yang diberikan oleh tutor</p>
+   <p style="text-align: center;">Semoga dilain waktu bisa belajar secara langsung dan ngopi bareng tentunya ya :D, udah gini aja ga bisa nulis dan ngarang yang bagus bagus :D </p>
+   <h2 style="text-align: center;">Fun Fact About Juli Kharisma </h2>
+   <p style="text-align: center;">1. Hayuk aja kalo diajak kemana </p>
+   <p style="text-align: center;">2. Yang seneng Hiking yuk kita agendakan</p>
+   <p style="text-align: center;">3. Ga ribet ribet dah orangnya </p>
+   <!--ul>
+       <li>Hayuk aja kalo diajak kemana</li>
+       <Li>Yang seneng Hiking yuk kita agendakan</Li>
+       <Li>Ga ribet ribet dah orangnya</Li>
+   </ul>
+   <!--Block-->
+   <div class="block"></div>
+   <!--inline-->
+   <span class="inline"></span>
+
+</body>
+</html>
